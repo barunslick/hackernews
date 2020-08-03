0813f370fe56d0995061525518d8d5938881f8bc
barunslick <barunpradhan987@gmail.com>
2020-08-03 09:59:26 +0545

    Updates

---
+++ precache-manifest.7d19a431d3ca9e8f5473036b64cb0ece.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "685515a41de04474534d870efb38a141",
+    "url": "/hackernews/index.html"
+  },
+  {
+    "revision": "60184b17258f146bf179",
+    "url": "/hackernews/static/css/main.7de2fcf8.chunk.css"
+  },
+  {
+    "revision": "627005d1e803e4a7f62f",
+    "url": "/hackernews/static/js/2.5bd96475.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hackernews/static/js/2.5bd96475.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60184b17258f146bf179",
+    "url": "/hackernews/static/js/main.c59f09c0.chunk.js"
+  },
+  {
+    "revision": "0217650cb7fc30d2a772",
+    "url": "/hackernews/static/js/runtime-main.cb44345c.js"
+  }
+]);
